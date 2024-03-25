b619ec53df636c003e7288749402dd7b0125e39d
joshua mwendwa <leeroyjoshu087@gmail.com>
2024-03-25 22:31:48 +0300

    initial install

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(theory) {
+    const winDetails=theory.find(item=>item["result"]==='W')
+    if (winDetails) {
+        return winDetails["year"]
+    }else{
+        return undefined
+    }
+}
